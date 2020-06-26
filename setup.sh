0497607a01dadb70bf0f94e4714d5db14d212a85
Sean Fields <sfields@worldventures.com>
2020-06-26 13:14:47 -0500

    no message

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 sudo apt-get update
 sudo apt-get install python-pyaudio portaudio19-dev
-wget "https://www.dropbox.com/s/x839y78nvqt9cbz/r2d2-sounds.tar.gz?dl=0"
+wget "https://www.dropbox.com/s/x839y78nvqt9cbz/r2d2-sounds.tar.gz"
 tar -xzvf r2d2-sounds.tar.gz
